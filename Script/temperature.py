e4f8769699c77f894b45e094dc0bbcd4712d5c31
MuhammadNauvalDwiAfandi <muhammadnauval270@gmail.com>
2022-08-13 18:22:03 +0700

    Merge main script

--- Script/temperature.py
+++ Script/temperature.py
@@ -35,3 +35,32 @@ def read_temp():
         temp_c = float(temp_string) / 1000.0
         temp_f = temp_c * 9.0 / 5.0 + 32.0
         return temp_c, temp_f
+
+def temp_chk():
+    print(' rom: '+ read_rom())
+    print('Mengukur suhu, tunggu 2 menit')
+    time.sleep(60)
+    print('Tunggu 1 menit lagi...')
+    time.sleep(30)
+    print('Tunggu 30 detik lagi...')
+    tmp = read_temp()
+    print(f"Your current temperature is {tmp[0]}C {tmp[1]}F")
+    return tmp[0]
+    
+def logic(tmp):
+    if tmp <= 36:
+        print("Error: Suhu terlalu rendah")
+    elif tmp > 36 and tmp <= 37.5:
+        print("Normal")
+    elif tmp > 37.5 and tmp <= 38.5:
+        print("Sakit ringan")
+    else:
+        print('Sakit parah')
+
+if __name__ == '__main__':
+    while True:
+        logic(temp_chk())
+        time.sleep(1)
+
+        input('Press enter key to continue...')
+
