bf790d4098e4412f9462c86d9b95ffc3d310e695
MuhammadNauvalDwiAfandi <muhammadnauval270@gmail.com>
2022-08-19 10:32:53 +0700

    Updated LED script to match board gpio

--- Script/sensor/LED.py
+++ Script/sensor/LED.py
@@ -1,34 +1,34 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
 def LEDGreen_On():
-    GPIO.setup(25,GPIO.OUT)
+    GPIO.setup(22,GPIO.OUT)
     # set GPIO-a pin to HIGH
-    GPIO.output(25,GPIO.HIGH)
+    GPIO.output(22,GPIO.HIGH)
     # pause for one second
     time.sleep(0.5)
 
 def LEDGreen_Off():
-    GPIO.setup(25,GPIO.OUT)
+    GPIO.setup(22,GPIO.OUT)
     # set GPIO-a pin to LOW
-    GPIO.output(25,GPIO.LOW)
+    GPIO.output(22,GPIO.LOW)
     # pause for one second
     time.sleep(0.5)
 
 def LEDRed_On():
-    GPIO.setup(8,GPIO.OUT)
+    GPIO.setup(24,GPIO.OUT)
     # set GPIO-a pin to LOW
-    GPIO.output(8,GPIO.HIGH)
+    GPIO.output(24,GPIO.HIGH)
     # pause for one second
     time.sleep(0.5)
 
 def LEDRed_Off():
-    GPIO.setup(8,GPIO.OUT)
+    GPIO.setup(24,GPIO.OUT)
     # set GPIO-a pin to LOW
-    GPIO.output(8,GPIO.LOW)
+    GPIO.output(24,GPIO.LOW)
     # pause for one second
     time.sleep(0.5)
 
